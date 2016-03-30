a1c617699ee492444c1e0c8a153e56e27bd33ea4
vdicairano91 <vdicairano91@gmail.com>
2016-03-30 17:22:56 -0400

    changed image names and moved them into images folder, added about, admin, and contact pages, updated styles

---
+++ .tmp/scripts/main.js
@@ -0,0 +1,6 @@
+"use strict";
+
+$("#footer").load("../common/footer.html");
+$("#subfooter").load("../common/subfooter.html");
+$("#nav").load("../common/nav.html");
+//# sourceMappingURL=main.js.map
